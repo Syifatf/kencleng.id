cbdf635849167ace7bdb9ada9ba4c09dc3eab1cf
Syifatf <syifatf@gmail.com>
2019-01-06 21:47:05 +0700

    menambahkan folder util dengan isi Consts.java

---
+++ app/src/main/java/syifa/app/kcid/util/Consts.java
@@ -0,0 +1,13 @@
+package syifa.app.kcid.util;
+
+/**
+ * Created by USER on 06/01/2019.
+ */
+
+public class Consts {
+
+    public static final String KEY_ACTION_DETAIL = "action";
+
+    public static final String INTENT_ADD = "add";
+    public static final String INTENT_EDIT = "edit";
+}
